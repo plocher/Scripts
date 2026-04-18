141bfbfb1189d5aaff695ddbd5dec48a3b6261a1
John Plocher <John.Plocher@gmail.com>
2026-04-18 12:15:32 -0700

    initial commit

---
+++ mvname.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# rename a set of files using a naming pattern
+# used in eagle workspaces to rename a whole set of files (board, schematic, gerbers, etc)
+# start with:
+#     foo.sch, foo.brd, foo.parts.txt foo1234.txt
+# mvname foo bar
+#     bar.sch, bar.brd, bar.parts.txt bar1234.txt
+#
+# John Plocher residing at gmail
+
+# set -x
+
+
+# usage:  $0 oldname newname
+recurse=0
+oname="$1"; shift
+if [ "$oname" = "-R" ]; then
+	recurse=1
+	oname="$1"; shift
+fi
+nname="$1"; shift
+
+
+do_rename() {
+	oname="$1"; shift
+	nname="$1"; shift
+	exts=`ls -1 --  "$oname"* | sed -e "s/^$oname//"`
+
+	for ext in $exts; do
+	    # (set -x;  mv -- "${oname}${ext}" "${nname}${ext}")
+	    mv -- "${oname}${ext}" "${nname}${ext}"
+	done
+}
+
+if [ $recurse -ne 0 ]; then
+	dirs=`find . -depth -type d -print | grep -v "^.$"`
+	for d in $dirs; do
+		(
+		cd $d
+		do_rename "$oname" "$nname"
+		)
+	done
+else
+	do_rename "$oname" "$nname"
+fi
+
